4e20b8f6c03f6bf8c7bdcd6af64144383c029e5b
Sergey Brazhnikov <serjak.2012@gmail.com>
2024-06-09 14:08:45 +0600

    lvl1 start, copy modal component from lesson, add button on app to open Modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,42 @@
 import {useState} from 'react';
 import './App.css';
+import Modal from './components/Modal/Modal';
 
 
 const App = () => {
   const [state, setState] = useState();
-
+  const [showModal,setShowModal]=useState(false);
 
   return (
     <>
-      dfsdf
+      <main className="container-fluid">
+        <div className="row mt-2">
+          <div className="col-4">
+
+          </div>
+
+          <div className="col-4">
+            <button className="w-100 btn btn-primary"
+                    onClick={() => setShowModal(true)}
+            >
+              Order
+            </button>
+          </div>
+          <div className="col-4">
+
+          </div>
+        </div>
+      </main>
+
+
+      <Modal show={showModal} title={'Order'} onClose={() => setShowModal(false)}>
+        <div className="modal-body">
+          content
+        </div>
+        <div className="modal-footer">
+          <button className="btn btn-danger" onClick={() => setShowModal(false)}>Cancel</button>
+        </div>
+      </Modal>
     </>
   );
 };
