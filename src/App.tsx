68461da13fab9ed7113f3915c3bb00af404c7271
Sergey Brazhnikov <serjak.2012@gmail.com>
2024-06-09 13:56:05 +0600

    create project, add bootstrap

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import {useState} from 'react';
+import './App.css';
+
+
+const App = () => {
+  const [state, setState] = useState();
+
+
+  return (
+    <>
+      dfsdf
+    </>
+  );
+};
+
+export default App;
